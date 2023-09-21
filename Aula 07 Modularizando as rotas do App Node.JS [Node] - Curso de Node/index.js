7a906c74e4c2e1a17a917924c4ec7ca0c5b5d7ed
and <andersonvittor1801@gmail.com>
2023-09-21 18:33:10 -0300

    feat(aula 07): adicionando rota users.js

--- Aula 07 Modularizando as rotas do App Node.JS [Node] - Curso de Node/index.js
+++ Aula 07 Modularizando as rotas do App Node.JS [Node] - Curso de Node/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const rotas = require('./rotas/rotas')
+const rotas = require('./rotas/cursos')
+const users = require('./rotas/users')
 const porta = process.env.PORT || 3000
 
 const app = express()
 
-app.use('/', rotas)
-app.get('*', (request, response) => {
-    response.send('CFB Cursos')
-})
+app.use('/cursos/', rotas)
+app.use('/users/', users)
 
 app.listen(porta, () => { console.log('Servidor rodando') })
